28765314b4474eb0113c587023f6c7680df9d4da
Nitewa1kr <adeel.khilji83@gmail.com>
2017-04-03 08:43:19 -0400

    mean stack tut with ng2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+import { PostsService } from './posts.service';
+
+const routes = [
+  { path: '', redirectTo: 'posts', pathMatch: 'full'},
+  { path: 'posts', component: PostsComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  declarations: [ AppComponent, PostsComponent ],
+  imports: [ BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(routes) ],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
